aec7db92283bb00e33487276f33ec8150ebee1a8
李中原 <lizhongyuan3@yeah.net>
2020-10-20 14:30:33 +0800

    请求头 swagger

--- lawfrim-web/src/main/java/com/xinou/lawfrim/web/config/SwaggerConfig.java
+++ lawfrim-web/src/main/java/com/xinou/lawfrim/web/config/SwaggerConfig.java
@@ -6,12 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zicheng
@@ -29,13 +35,26 @@ public class SwaggerConfig {
      */
     @Bean
     public Docket createRestApi() {
+        List<Parameter> pars = new ArrayList<>();
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        pars.add(
+                ticketPar
+                        .name("token")
+                        .description("登陆所需token")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(true)
+                        .build()
+        );
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .useDefaultResponseMessages(false)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.xinou"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
